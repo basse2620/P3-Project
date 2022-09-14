747918cf6133e502d86d825c6967404dca0365b9
basse2620 <88773780+basse2620@users.noreply.github.com>
2022-09-14 13:12:26 +0200

    Delete instruktoer.js

--- instruktoer.js
+++
@@ -1,8 +0,0 @@
-class Instruktoer {
-    constructor(PK_instruktoerId, fornavn, efternavn) {
-        this.PK_instruktoerId = PK_instruktoerId;
-        this.fornavn = fornavn;
-        this.efternavn = efternavn;
-    }
-}
-module.exports = Instruktoer;
