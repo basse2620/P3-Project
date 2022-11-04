dd368925aeead4a4b5ce6b9724ed5e937a741979
basse2620 <bassemunch@gmail.com>
2022-11-04 10:28:41 +0100

    interfaces

--- angularProjekt/src/app/interface/laane-film-instruktoer.ts
+++ angularProjekt/src/app/interface/laane-film-instruktoer.ts
@@ -1,2 +1,4 @@
 export interface LaaneFilmInstruktoer {
+    FK_filmId: number;
+    FK_instruktoerId: number;
 }
--- angularProjekt/src/app/interface/laane-ordre-detaljer.ts
+++ angularProjekt/src/app/interface/laane-ordre-detaljer.ts
@@ -1,2 +1,9 @@
 export interface LaaneOrdreDetaljer {
+    FK_ordreId: number;
+    FK_filmId: number;
+    pris: number;
+    rabat: number;
+    totalPris: number;
+    udlaansDato: Date;
+    returDato: Date;
 }
