be63897f46b8b17fe5a8995b90f1c5feb55533c9
basse2620 <bassemunch@gmail.com>
2022-11-08 07:43:46 +0100

    sercieses

--- angularProjekt/src/app/services/bruger/bruger.service.ts
+++ angularProjekt/src/app/services/bruger/bruger.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Bruger } from '../../interfaces/bruger';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrugerService {
+  private apiUrl = 'http://192.168.20.30:8090/bruger';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBrugere(): Observable<Bruger[]> {
+    return this.http.get<Bruger[]>(this.apiUrl);
+  }
+
+  getBruger(bruger: Bruger): Observable<Bruger> {
+    const url = `${this.apiUrl}/${bruger.PK_brugernavn}/${bruger.password}`;
+    return this.http.get<Bruger>(url);
+  }
+
+  postBruger(bruger: Bruger): Observable<Bruger> {
+    return this.http.post<Bruger>(this.apiUrl, bruger, httpOptions);
+  }
+
+  patchBruger(bruger: Bruger): Observable<Bruger> {
+    return this.http.post<Bruger>(this.apiUrl, bruger, httpOptions)
+  }
 }
--- angularProjekt/src/app/services/instruktoer/instruktoer.service.ts
+++ angularProjekt/src/app/services/instruktoer/instruktoer.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Instruktoer } from '../../interfaces/instruktoer';
+import { LaaneFilm } from '../../interfaces/laane-film';
+import { SalgsFilm } from '../../interfaces/salgs-film';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class InstruktoerService {
+export class GenreService {
+  private apiUrl = 'http://192.168.20.30:8090/instruktoer';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getGenre(): Observable<Instruktoer[]> {
+    return this.http.get<Instruktoer[]>(this.apiUrl);
+  }
+
+  getGenren(instruktoer: Instruktoer): Observable<Instruktoer> {
+    const url = `${this.apiUrl}/${instruktoer.PK_instruktoerId}`;
+    return this.http.get<Instruktoer>(url);
+  }
+
+  getGenreLaaneFilm(laaneFilm: LaaneFilm): Observable<Instruktoer> {
+    const url = `${this.apiUrl}/${laaneFilm.PK_filmId}`;
+    return this.http.get<Instruktoer>(url);
+  }
+
+  getGenreSalgsFilm(salgsFilm: SalgsFilm): Observable<Instruktoer> {
+    const url = `${this.apiUrl}/${salgsFilm.PK_filmId}`;
+    return this.http.get<Instruktoer>(url);
+  }
 }
